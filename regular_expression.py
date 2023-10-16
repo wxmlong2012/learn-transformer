3b29e4d51c9e5e5606679a8724f248cf9d8e9633
wxmlong2012 <44708161+wxmlong2012@users.noreply.github.com>
2023-10-16 09:50:57 -0400

    Update regular_expression.py

--- regular_expression.py
+++ regular_expression.py
@@ -1,4 +1,3 @@
-
 import re
 
 # put angles around all numbers
@@ -61,3 +60,85 @@ pattern2 = r"apple(?= pie)"
 re.findall(pattern1, text)
 # should find one
 re.findall(pattern2, text)
+
+# match telephon number ###-###-####
+text = "Here is my telephone cellphone 984-209-8878, this is also my number: 9842098878 "
+pattern1 = r"\d{3}-\d{3}-\d{4}"
+pattern2 = r"\d{3}-\d{3}-\d{4}|\d{10}"
+re.findall(pattern1, text)
+re.findall(pattern2, text)
+
+# match the first occurrence from the beginning, not in the middle
+re.match(pattern2, text)
+# match the first occurrence any where in the string
+matched = re.search(pattern2, text)
+matched.group()
+
+# match gruppy or grappies
+# findall only find the groups, not the whole text?
+text = "it is gruppy or gruppies"
+pattern = r"grupp(y|ies)"
+pattern1 = r"grupp(?:y|ies)"
+pattern2 = r"gruppy"
+re.findall(pattern, text)
+re.search(pattern, text)
+re.search(pattern, text).group()
+
+re.findall(pattern1, text)
+re.search(pattern1, text)
+
+re.findall(pattern2, text)
+
+# In the first pattern, we match the entire date as a whole.
+# In the second pattern, we capture the day, month, and year separately.
+# You can see the difference between matching and capturing in the output.
+
+text = "Today's date is 14-10-2023."
+
+# Match the entire date pattern
+pattern = r"\d{2}-\d{2}-\d{4}"
+match = re.search(pattern, text)
+
+if match:
+    matched_text = match.group()
+    print("Matched:", matched_text)
+else:
+    print("No match")
+
+# Capture the day, month, and year individually
+pattern = r"(\d{2})-(\d{2})-(\d{4})"
+match = re.search(pattern, text)
+
+if match:
+    day = match.group(1)
+    month = match.group(2)
+    year = match.group(3)
+    print("Captured:")
+    print("Day:", day)
+    print("Month:", month)
+    print("Year:", year)
+else:
+    print("No match")
+
+
+text = "My phone number is 123-456-7890" #, and my other number is 987-654-3210."
+pattern = r"is (\d{3})-(\d{3})-(\d{4})"
+matches = re.findall(pattern, text)
+print(matches)
+
+
+import nltk
+text = 'That U.S.A. poster-print costs $12.40...'
+# note here ?: match but not capture the group
+pattern = r"""(?x)                 # set flag to allow verbose regexps
+              (?:[A-Z]\.)+           # abbreviations, e.g. U.S.A.
+              |\w+(?:-w+)*           # words with optional internal hyphens
+              |\$?\d+(?:\.\d+)?%?    # currency and percentages, e.g. $12.40, 82%
+              |\.\.\.              # ellipsis
+              |[][.,;?():-_]       # these are separate tokens; includes ], [ . , ( ) etc
+"""
+tokens = nltk.regexp_tokenize(text, pattern)
+print(tokens)
+
+
+
